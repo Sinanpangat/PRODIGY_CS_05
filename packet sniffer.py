82c6a1d5831a7298a3aaea920da49a5e959bd697
Sinanpangat <sinanpangat1@gmail.com>
2024-08-23 01:20:17 +0530

    Add files via upload

--- packet sniffer.py
+++ packet sniffer.py
@@ -2,16 +2,26 @@ from scapy.all import *
 
 def packet_handler(packet):
     """Handles captured packets."""
+    answer=""
     if packet.haslayer(IP):
         src_ip = packet[IP].src
         dst_ip = packet[IP].dst
         protocol = packet[IP].proto
-        print(f"Source IP: {src_ip}")
-        print(f"Destination IP: {dst_ip}")
-        print(f"Protocol: {protocol}")
-
+        if protocol == 1:  # ICMP
+            answer=packet[ICMP].summary()
+            print(answer)
+        elif protocol == 6:  # TCP
+            answer=packet[TCP].summary()
+            print(answer)
+        elif protocol == 17:  # UDP
+            answer=packet[UDP].summary()
+            print(answer)
+        else:
+            print("Payload Data:", packet.summary())
+        print("Source IP: ",src_ip,"Destination IP:",dst_ip,"Protocol:",protocol,"\n")
+        if packet.haslayer("Raw"):
+            payload = packet[Raw].load
+            print("data: ",payload,"\n")
         # Extract payload data based on the protocol
         
-
-if __name__ == "__main__":
-    sniff(prn=packet_handler, filter="ip")
+sniff(prn=packet_handler, filter="ip")
